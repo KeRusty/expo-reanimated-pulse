66ad01ad11f20c6d13b5992ef3945ffde821eaf4
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-04-17 10:27:59 +0530

    Create home-styles.js

---
+++ src/views/home/home-styles.js
@@ -0,0 +1,18 @@
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#d3d3d3',
+    },
+    topContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: 250
+    }
+})
