90eff5b723cdc8bfb15f4bbc80a06ccf8b5238d4
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-04-17 09:55:07 +0530

    compartmentalized

---
+++ src/views/home-styles.js
@@ -0,0 +1,12 @@
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#f1f1f1',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
